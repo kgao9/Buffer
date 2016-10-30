2aea5d21522a2a25f95b4c93db99d0c012f5dec2
Kenny Gao <kgao9@wisc.edu>
2016-10-30 17:03:01 -0500

    Finished 4 methods - advanceClock, allocBuf, unpinPage, allocPage All methods work as expected, except for error handling Documentation is at pseudo code level - gonna need some work Note: I'm implmenting this using test-driven development - so I might add more tests if I have time

--- BufMgr/src/buffer.cpp
+++ BufMgr/src/buffer.cpp
@@ -35,16 +35,79 @@ BufMgr::BufMgr(std::uint32_t bufs)
 }
 
 
-BufMgr::~BufMgr() {
+BufMgr::~BufMgr() 
+{
 }
 
 void BufMgr::advanceClock()
 {
+    //std::cout << "advancing";
 
+    if(clockHand == numBufs - 1)
+    {
+        clockHand = 0;
+    }
+
+    else
+    {
+        clockHand++;
+    }
 }
 
 void BufMgr::allocBuf(FrameId & frame) 
 {
+    //if we go all the way around, throw error
+    FrameId currentFrameNo = clockHand;
+
+    //tell if unallocated frame
+    int found = 0;
+    int first = 0;
+    int noPinned = 1;
+
+    //while not found
+    while(found == 0)
+    {
+
+        //no free frames
+        if(first != 0 && clockHand == currentFrameNo && noPinned == 1)
+        {
+            std::cout << "error\n";
+            exit(0);
+        }
+
+        //get current frame
+        BufDesc currentFrame = bufDescTable[clockHand];
+        
+        //currently pinned, advance clock
+        if(currentFrame.pinCnt > 0)
+        {
+            advanceClock();
+        }
+
+        //second case is ref bit is set
+        //if set, we want to unset it
+        else if(currentFrame.refbit == true)
+        {
+            //there exists some frame that wasn't pinned, so we never want to throw
+            //an exception
+            noPinned = 0;
+
+            //advance
+            currentFrame.refbit = false;
+            bufDescTable[clockHand] = currentFrame;
+            advanceClock();
+        }
+
+        //empty
+        else
+        {
+            found = 1;
+            frame = clockHand;
+            advanceClock();
+        }
+
+        first = 1;
+    }
 }
 
 	
@@ -55,6 +118,33 @@ void BufMgr::readPage(File* file, const PageId pageNo, Page*& page)
 
 void BufMgr::unPinPage(File* file, const PageId pageNo, const bool dirty) 
 {
+    //this is the frame id
+    FrameId getFrameIdx;
+
+    //look up where the frame is
+    hashTable -> lookup(file, pageNo, getFrameIdx);
+
+    std::cout << "checking\n";
+
+    //get that frame
+    BufDesc getFrame = bufDescTable[getFrameIdx];
+
+    if(getFrame.pinCnt == 0)
+    {
+        std::cout << "PNP error: page isn't pinned\n";
+        exit(0);
+    }
+
+    //decrement pin
+    getFrame.pinCnt = (getFrame.pinCnt - 1);
+  
+    if(dirty == true)
+    {
+        getFrame.dirty = true;
+    }
+
+    //update
+    bufDescTable[getFrameIdx] = getFrame;
 }
 
 void BufMgr::flushFile(const File* file) 
@@ -63,6 +153,27 @@ void BufMgr::flushFile(const File* file)
 
 void BufMgr::allocPage(File* file, PageId &pageNo, Page*& page) 
 {
+    //allocate page
+    Page emptyPage = file -> allocatePage();
+
+    //get corresponding page number
+    pageNo = emptyPage.page_number();
+
+    FrameId getFrame;
+    allocBuf(getFrame);
+
+    std::cout << "here\n";
+    hashTable -> insert(file, pageNo, getFrame);
+    std::cout << "Not here\n";
+    BufDesc currentFrame = bufDescTable[getFrame];
+    currentFrame.Set(file, pageNo);
+
+    //update frame information
+    bufDescTable[getFrame] = currentFrame;
+
+    //update page info
+    bufPool[getFrame] = emptyPage;
+    page = &(bufPool[getFrame]);
 }
 
 void BufMgr::disposePage(File* file, const PageId PageNo)
